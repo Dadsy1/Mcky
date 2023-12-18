d3034cf699b4e5270f6248461cdbbe9fddaf682f
Mcky <152825387+Dadsy1@users.noreply.github.com>
2023-12-18 10:45:04 +0700

    ..
    ...

---
+++ run.py
@@ -0,0 +1,17 @@
+from lineQR import *
+from datetime import datetime
+import asyncio, json, sys, time
+
+############## Line QR ##############
+cl = lineQR()
+clset = cl.lineQr("IOSIPAD")
+cl.apiJsonPrint(clset)
+clpin = clset["Key"]
+pp = cl.linePin(clpin)
+cl.apiJsonPrint(pp)
+cltk = cl.lineToken(clpin)
+cl.apiJsonPrint(cltk)
+token = cltk
+print (f'Token Line : {str(token)}')
+
+#----------------------------------------------------------------------------
