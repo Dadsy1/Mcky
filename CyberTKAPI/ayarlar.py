35fa4a369629e4c261a16dc6960b64b6e1185db1
Mcky <152825387+Dadsy1@users.noreply.github.com>
2023-12-18 12:32:42 +0700

    .
    .

---
+++ CyberTKAPI/ayarlar.py
@@ -0,0 +1,30 @@
+
+
+
+class Ayarlar(object):
+    
+    
+    ####### HOST ##########
+    
+    LINE_QR_HOST = "https://qr.cybertkr.com"
+    LINE_LIFF_HOST = "https://liff.cybertkr.com"
+    
+    
+    
+    
+    
+    
+    ####### ENDPOINT ##########
+    
+    LINE_QR_ENDPOINT = "/link"
+    LINE_PINCODE_ENDPOINT = "/pincode"
+    LINE_ATOKEN_ENDPOINT = "/authToken"
+    LINE_LIFF_ENDPOINT = "/liffapproval"
+    
+    
+    
+    ####### INIT AND TOOLS ##########
+    
+    def __init__(self):
+        self.Hcl58y0 = {}
+        self.v291jqA = {}
